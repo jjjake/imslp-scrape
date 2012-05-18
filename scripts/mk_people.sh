255630dc8dc3123239dabef6242e64c48ebac98b
Jake Johnson <jake@archive.org>
2012-05-18 22:21:25 +0000

    log URL if everything fails

--- scripts/mk_people.sh
+++ scripts/mk_people.sh
@@ -3,12 +3,13 @@ do
     id=$(curl -s $link | egrep -o  'oldid=[0-9]{5,6}' | egrep -o '[0-9]{5,6}' | sort -u)
     if [ ! -f ./people/$id/$id.html ] 
     then
-        echo "link: $link\tid: $id" >> mk_people.log
         mkdir -p ./people/$id
         curl -s $link > ./people/$id/$id.html
-    fi
-    if [ -z $id ]
+    elif [ -z $id ]
     then
         echo "ERROR :: $link has no ID!" >> mk_people.log
+    else
+
+        echo "ERROR :: $link couldn't create item!" >> mk_people.log
     fi
 done
