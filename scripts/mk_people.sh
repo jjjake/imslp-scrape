ef9a514050d1f4c556b9238868ebababb2afce85
Jake Johnson <jake@archive.org>
2012-05-18 20:59:21 +0000

    added missing id logging

--- scripts/mk_people.sh
+++ scripts/mk_people.sh
@@ -1,6 +1,14 @@
 for link in `cat people_pages.txt`
 do
     id=$(curl -s $link | egrep -o  'oldid=[0-9]{6}' | egrep -o '[0-9]{6}' | sort -u)
-    mkdir -p ./people/$id
-    curl -s $link > ./people/$id/$id.html
+    if [ ! -f ./people/$id/$id.html ] 
+    then
+        echo "link: $link\tid: $id" >> mk_people.log
+        mkdir -p ./people/$id
+        curl -s $link > ./people/$id/$id.html
+    fi
+    if [ -z $id ]
+    then
+        echo "ERROR :: $id has no ID!" >> mk_people.log
+    fi
 done
