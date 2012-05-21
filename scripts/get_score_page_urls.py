1a55bd290733de24348a0447177041c326f73b20
Jake Johnson <jake@archive.org>
2012-05-21 19:03:16 +0000

    download score/item html, create score-id => score url map

---
+++ scripts/get_score_page_urls.py
@@ -0,0 +1,34 @@
+#!/home/jake/.virtualenvs/default/bin/python
+import os
+import urllib2
+import lxml.html
+import re
+
+
+def parse_html(html):
+    return lxml.html.fromstring(open(html).read())
+
+def get_item_links(parsed_html):
+    try:
+        scores = parsed_html.get_element_by_id('mw-pages').iterlinks()
+        return [x[2] for x in scores]
+    except KeyError:
+        return None
+
+for directory in os.listdir('people'):
+    id_pattern = re.compile(r'oldid=\d{5,6}')
+    html_file = "people/%s/%s.html" % (directory, directory)
+    parsed_html = parse_html(html_file)
+    if get_item_links(parsed_html):
+        item_links = get_item_links(parsed_html)
+        for url in item_links:
+            url = "http://imslp.org%s" % url
+            html = urllib2.urlopen(url).read()
+            oldid = re.search(id_pattern, html).group()
+            if oldid: id = oldid.replace('oldid=', '')
+            scores_html = "people/%s/%s_scores.html" % (directory, id)
+            score_id_map = "score_id_to_url_map.txt"
+            with open(scores_html, "wb") as f:
+                f.write(html)
+            with open(score_id_map, "a") as ff:
+                ff.write(id + '\t' + url + '\n')
