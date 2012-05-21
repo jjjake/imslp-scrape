7674a2e302d5725639fa70d17370f8ad8a33aeb1
Jake Johnson <jake@archive.org>
2012-05-21 21:35:02 +0000

    fixed directory movement

--- scripts/get_pdf.py
+++ scripts/get_pdf.py
@@ -5,6 +5,7 @@ import lxml.html
 import re
 from subprocess import call
 
+ROOT_DIR = os.path.join(os.getcwd(), 'people')
 
 def parse_html(html):
     return lxml.html.fromstring(open(html).read())
@@ -13,17 +14,18 @@ def get_item_links(parsed_html):
     return [x[2] for x in parsed_html.iterlinks() if 'pdf' in x[2] and 'images' in x[2]]
 
 for dir in os.listdir('people'):
-    print '\n\n---\n\n'
-    for file in os.listdir('people/%s' % dir):
+    os.chdir(ROOT_DIR)
+    ITEM_DIR = os.path.join(ROOT_DIR, dir)
+    os.chdir(ITEM_DIR)
+    for file in os.listdir('.'):
         if file.endswith('scores.html'):
-            file = 'people/%s/%s' % (dir, file)
+            file = os.path.join(ITEM_DIR, file)
             parsedhtml = parse_html(file)
             item_links = get_item_links(parsedhtml)
-            mapfname = 'people/%s/map.txt' % dir
             for pdf in item_links:
                 pdf_url = 'http://imslp.org%s' % pdf
-                wget = "wget -nc --header='Cookie: imslpdisclaimeraccepted=yes' %s" % pdf_url
+                wget = "wget -q -nc --header='Cookie: imslpdisclaimeraccepted=yes' %s" % pdf_url
                 call(wget, shell=True)
-                with open(mapfname, 'a') as mf:
+                with open('map.txt', 'a') as mf:
                     mf.write(file + '\t' + pdf_url + '\n')
                 print "SUCCESSS :: %s\t%s" % (dir, pdf_url)
