e9848e49d9658239e3744d38fb074782ca4ecc0e
Jake Johnson <jake@archive.org>
2012-05-22 18:55:48 +0000

    cleaned up

--- scripts/get_score_page_urls.py
+++ scripts/get_score_page_urls.py
@@ -16,19 +16,27 @@ def get_item_links(parsed_html):
         return None
 
 for directory in os.listdir('people'):
-    id_pattern = re.compile(r'oldid=\d{5,6}')
+    id_pattern = re.compile(r'oldid=\d{5,7}')
     html_file = "people/%s/%s.html" % (directory, directory)
-    parsed_html = parse_html(html_file)
-    if get_item_links(parsed_html):
-        item_links = get_item_links(parsed_html)
-        for url in item_links:
-            url = "http://imslp.org%s" % url
-            html = urllib2.urlopen(url).read()
-            oldid = re.search(id_pattern, html).group()
-            if oldid: id = oldid.replace('oldid=', '')
-            scores_html = "people/%s/%s_scores.html" % (directory, id)
-            score_id_map = "score_id_to_url_map.txt"
-            with open(scores_html, "wb") as f:
-                f.write(html)
-            with open(score_id_map, "a") as ff:
-                ff.write(id + '\t' + url + '\n')
+    ffiles = [x for x in os.listdir('people/%s' % directory) if 'scores' in x]
+    if ffiles:
+        continue
+    ffiles = []
+    try:
+        parsed_html = parse_html(html_file)
+        if get_item_links(parsed_html):
+            item_links = get_item_links(parsed_html)
+            for url in item_links:
+                url = "http://imslp.org%s" % url
+                html = urllib2.urlopen(url).read()
+                oldid = re.search(id_pattern, html)
+                if oldid: id = oldid.group().replace('oldid=', '')
+                scores_html = "people/%s/%s_scores.html" % (directory, id)
+                score_id_map = "score_id_to_url_map.txt"
+                with open(scores_html, "wb") as f:
+                    f.write(html)
+                with open(score_id_map, "a") as ff:
+                    ff.write(id + '\t' + url + '\n')
+    except:
+        print 'ERROR :: %s' % directory
+        continue
